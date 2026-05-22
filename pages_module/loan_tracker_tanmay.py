4b08c61fbe686a1f66f4308309b135eba5569120
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-05-22 22:21:38 +0530

    Update loan_tracker_tanmay.py

--- pages_module/loan_tracker_tanmay.py
+++ pages_module/loan_tracker_tanmay.py
@@ -3,4 +3,9 @@ import streamlit as st
 def render():
     st.title("💸 Loan Tracker by Tanmay")
     
-    st.tabs(["loan config", "emi table"])
+    tab1, tab2 = st.tabs(["loan config", "emi table"])
+    
+    with tab1:
+        principal = st.number_input("Principal Amount")
+        rate_of_interest = st.number_input("Rate of Interest")
+        emi = st.number_input("EMI")

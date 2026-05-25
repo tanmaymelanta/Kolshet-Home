1ae5fc60affd7830a797f58464832d44683e53b2
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-05-25 21:06:21 +0530

    Update loan_tracker_tanmay.py

--- pages_module/loan_tracker_tanmay.py
+++ pages_module/loan_tracker_tanmay.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def emi(principal,rate_of_interest, tenure):
-    monthly_roi = rate_of_interest / 12 * 100
+    monthly_roi = rate_of_interest / 12 / 100
     emi = (principal * monthly_roi * (1 + monthly_roi) * tenure) / ((1 + monthly_roi) * tenure - 1)
     return emi
 
@@ -11,9 +11,11 @@ def render():
     
     emi_no = []
     for i in range(360):
-        emi_no.append(i+1)
+        month_no.append(i+1)
+        emi_amount.append(emi(principal= 5800000,rate_of_interest= 7.65, tenure= 360-i+1))
     
     df = pd.DataFrame({
-        "Month": emi_no
+        "Month": month_no,
+        "EMI": emi_amount
     })
     st.dataframe(df, hide_index=True)

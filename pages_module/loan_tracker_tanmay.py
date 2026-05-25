8154cc8831d04b721f1b4852aed593380073656b
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-05-25 20:47:48 +0530

    Update loan_tracker_tanmay.py

--- pages_module/loan_tracker_tanmay.py
+++ pages_module/loan_tracker_tanmay.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+def emi(principal,rate_of_interest, tenure):
+    monthly_roi = rate_of_interest / 12 * 100
+    emi = (principal * monthly_roi * (1 + monthly_roi) * tenure) / ((1 + monthly_roi) * tenure - 1)
+    return emi
 
 def render():
     st.title("💸 Loan Tracker by Tanmay")
     
-    tab1, tab2 = st.tabs(["loan config", "emi table"])
-    
-    with tab1:
-        principal = st.number_input("Principal Amount")
-        rate_of_interest = st.number_input("Rate of Interest")
-        emi = st.number_input("EMI")
+    df = pd.DataFrame()

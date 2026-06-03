0a5aa0225ac55387e7a7fc68d39c84deefe9f43f
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-06-03 12:54:52 +0530

    Update loan_tracker_test.py

--- pages_module/loan_tracker_test.py
+++ pages_module/loan_tracker_test.py
@@ -21,7 +21,8 @@ def render():
   col3.metric("Interest Paid", f"₹{total_interest_paid:,.0f}")
 
   st.divider()
-  st.line_chart(filtered_df.set_index("Month-Year")["Closing Balance"])
+  st.line_chart(filtered_df, x="Month-Year", y="Closing Balance", color='col3')
+  # st.line_chart(filtered_df.set_index("Month-Year")["Closing Balance"])
 
   st.divider()
   st.dataframe(df)

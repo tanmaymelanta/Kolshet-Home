9c31bf76f739cb13fd551d6728632bcfe7d9b7ec
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-06-03 12:40:50 +0530

    Update loan_tracker_test.py

--- pages_module/loan_tracker_test.py
+++ pages_module/loan_tracker_test.py
@@ -5,4 +5,6 @@ def render():
   st.write("loan tracket testing")
   url = "https://docs.google.com/spreadsheets/d/1Sh5-kymrGcPSm8D5e1B1jUB40q0Mel9crvkrBiDFKmc/export?format=csv"
   df = pd.read_csv(url)
+  filtered_df = df[df["Status"] == "Paid"].reset_index(drop=True)
+  st.dataframe(filtered_df)
   st.dataframe(df)

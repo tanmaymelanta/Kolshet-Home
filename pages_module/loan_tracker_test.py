2fbfd823a236f56daa70e7a68b2219668f15a43b
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-06-03 12:21:34 +0530

    Create loan_tracker_test.py

---
+++ pages_module/loan_tracker_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+
+def render():
+  st.write("loan tracket testing")
+  url = "https://docs.google.com/spreadsheets/d/1Sh5-kymrGcPSm8D5e1B1jUB40q0Mel9crvkrBiDFKmc/export?format=csv"
+  df = pd.read_csv(url)
+  st.dataframe(df)

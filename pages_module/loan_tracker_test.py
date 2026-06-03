68d2f00927d654b36ead8e8566fb0e7bdefb716b
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-06-03 12:43:09 +0530

    Update loan_tracker_test.py

--- pages_module/loan_tracker_test.py
+++ pages_module/loan_tracker_test.py
@@ -6,5 +6,14 @@ def render():
   url = "https://docs.google.com/spreadsheets/d/1Sh5-kymrGcPSm8D5e1B1jUB40q0Mel9crvkrBiDFKmc/export?format=csv"
   df = pd.read_csv(url)
   filtered_df = df[df["Status"] == "Paid"].reset_index(drop=True)
-  st.dataframe(filtered_df)
+
+  current_balance = filtered_df["Closing Balance"].iloc[-1]
+  total_interest_paid = filtered_df["Interest Paid"].sum()
+  total_principal_paid = filtered_df["Principal Paid"].sum()
+
+  col1, col2, col3 = st.columns(3)
+  col1.metric("Outstanding Balance", f"₹{current_balance:,.0f}")
+  col2.metric("Principal Paid", f"₹{total_principal_paid:,.0f}")
+  col3.metric("Interest Paid", f"₹{total_interest_paid:,.0f}")
+  
   st.dataframe(df)
